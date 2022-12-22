4515eebdaa5dd41818be2eaf0f5a299f8d0adfbe
Dmitry <profydima@yandex.ru>
2022-12-22 09:30:32 +0300

    загрузил 4 задачу

--- Program.cs
+++ Program.cs
@@ -44,3 +44,23 @@ if (no)
 }
 
 // 4 задача
+int n = 1;
+bool no = true;
+
+Console.WriteLine ("Введите число");
+int N = int.Parse (Console.ReadLine()!);
+
+ Console.WriteLine ("Чётные числа от 1 до " + N);
+ while (n < N)
+{ 
+    if (n % 2 == 0)
+{
+    Console.Write (n + ",");
+    no = false;
+}
+n++;
+}
+if (no)
+{
+    Console.WriteLine ("Нет четных чисел");
+}
