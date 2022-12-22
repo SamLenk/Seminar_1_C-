a373eaf4d03c40574ec03fc0ec312e48f87ad043
Dmitry <profydima@yandex.ru>
2022-12-22 07:30:53 +0300

    1 Zadacha

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Введите два числа");
+int num1 = int.Parse(Console.ReadLine()!);
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num1 > num2)
+                Console.WriteLine($"Максимальное {num1}");
+            else 
+                Console.WriteLine($"Максимальное {num2}");
