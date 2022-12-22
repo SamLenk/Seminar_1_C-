3e49a83f7e7dd6e8ab0c8e05c62bede00dcf07cd
Dmitry <profydima@yandex.ru>
2022-12-22 08:44:58 +0300

    Сделали все задачи

--- Program.cs
+++ Program.cs
@@ -1,13 +1,10 @@
-﻿Console.WriteLine("Введите три числа");
+﻿Console.WriteLine("Введите два числа");
 int num1 = int.Parse(Console.ReadLine()!);
 int num2 = int.Parse(Console.ReadLine()!);
-int num3 = int.Parse(Console.ReadLine()!);
 
-if (num1 > num2 && num1 > num3)
-                    Console.WriteLine("Максимальное = " + num1); 
-                else if (num2 > num3)
-                    Console.WriteLine("Максимальное = " + num2); 
-                else
-                    Console.WriteLine("Максимальное = " + num3); 
+if (num1 > num2)
+                Console.WriteLine($"Максимальное {num1}");
+            else 
+                Console.WriteLine($"Максимальное {num2}");
 
-   
+
