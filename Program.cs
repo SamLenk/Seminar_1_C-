0c9edee2c5b981495269171dee90f7d90cfa338e
Dmitry <profydima@yandex.ru>
2022-12-22 09:30:11 +0300

    загрузил 3 задачу

--- Program.cs
+++ Program.cs
@@ -22,3 +22,25 @@ if (num1 > num2 && num1 > num3)
                     Console.WriteLine("Максимальное = " + num3); 
 
 // 3 задача
+int n = 1;
+bool no = true;
+
+Console.WriteLine ("Введите число");
+int N = int.Parse (Console.ReadLine()!);
+
+ Console.WriteLine ("Чётные числа от 1 до " + N);
+ while (n < N)
+{ 
+    if (n % 2 == 0)
+{
+    Console.Write (n + ",");
+    no = false;
+}
+n++;
+}
+if (no)
+{
+    Console.WriteLine ("Нет четных чисел");
+}
+
+// 4 задача
